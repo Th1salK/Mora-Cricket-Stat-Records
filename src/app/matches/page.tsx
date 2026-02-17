d1fcf6222b0acf5eef243c4c1ad6bf62175120f0
Thisal Kokuhennadi <thisalkh22@gmail.com>
2026-02-18 00:02:47 +0530

    Test change

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -1,27 +1,16 @@
-import { json } from 'stream/consumers'
 import MatchesClient from '../../components/MatchesClient'
-
-type Match = {
-  _id?: string
-  date: string
-  opponent: string
-  venue: 'Home' | 'Away'
-  overs: number
-  matchType: 'Home and Home' | 'Practice' | 'Div 3'|'Inter Uni' | 'SLUG'
-}
+import { connectDB } from '../../lib/mongodb'
+import Match from '../../models/Match'
 
 export default async function Page() {
+  await connectDB()
 
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
-
-  const res = await fetch(`${baseUrl}/api/matches`, { cache: 'no-store' })
-  const data = await  res.json()
-  const matches: Match[] = await res.json()
+  const matches = await Match.find().sort({ date: -1 }).lean()
 
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-4">Matches</h1>
-      <MatchesClient matches={matches} />
+      <MatchesClient matches={JSON.parse(JSON.stringify(matches))} />
     </div>
   )
 }
