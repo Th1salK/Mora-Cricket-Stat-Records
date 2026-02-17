b65b191f2ed16b671065360aa633737e4caf927b
Thisal Kokuhennadi <thisalkh22@gmail.com>
2026-02-17 23:21:16 +0530

    defined baseUrl

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -10,7 +10,10 @@ type Match = {
 }
 
 export default async function Page() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/matches`, { cache: 'no-store' })
+
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
+  const res = await fetch(`${baseUrl}/api/matches`, { cache: 'no-store' })
   const data = await  res.json()
   const matches: Match[] = data.matches ?? []
 
